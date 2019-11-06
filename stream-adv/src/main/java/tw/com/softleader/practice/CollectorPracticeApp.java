81e76a5450219e9430ffe6a5adc40f1650172aea
forev <forev@LAPTOP-AHLENS2S>
2019-11-06 17:46:21 +0800

    寫完yearmonth

--- stream-adv/src/main/java/tw/com/softleader/practice/CollectorPracticeApp.java
+++ stream-adv/src/main/java/tw/com/softleader/practice/CollectorPracticeApp.java
@@ -1,9 +1,11 @@
 package tw.com.softleader.practice;
 
-import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
+import java.time.temporal.WeekFields;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import tw.com.softleader.sample.data.TimeClockDatas;
@@ -25,53 +27,68 @@ public class CollectorPracticeApp {
 	 * 以人名分組
 	 */
 	public static void groupingByName(List<TimeClockEntity> timeClocks) {
-		Map<String,List<TimeClockEntity>> map=timeClocks.stream()
-		.collect(Collectors.groupingBy(TimeClockEntity::getEmployeeName));
+		Map<String, List<TimeClockEntity>> map = new HashMap<>();
+		for (TimeClockEntity t : timeClocks) {
+			if (!map.containsKey(t.getEmployeeName())) {
+				List<TimeClockEntity> list = new ArrayList<>();
+				list.add(t);
+				map.put(t.getEmployeeName(), list);
+			} else {
+				map.get(t.getEmployeeName()).add(t);
+			}
+		}
+
+//				 Map<String, List<TimeClockEntity>> map = timeClocks.stream()
+//				.collect(Collectors.groupingBy(TimeClockEntity::getEmployeeName));
 		System.out.println(map);
 	}
 
 	/**
-	 * 以人名分組
-	 * 並算出每個人的合計薪資
+	 * 以人名分組 並算出每個人的合計薪資
 	 */
 	public static void groupingByNameWithSumSalary(List<TimeClockEntity> timeClocks) {
-//		Map<String,List<TimeClockEntity>> map=timeClocks.stream()
-//		.collect(Collectors.groupingBy(TimeClockEntity::getEmployeeName));
-//		map.forEach((K,V)->{
-//			double result=0;
-//			for(TimeClockEntity a:V) {
-//				result=a.calcSalary();
-//			}
-//			System.out.println(result);
-//		});
-		Map<String,Double> map2=timeClocks.stream()
-				.collect(Collectors.groupingBy(TimeClockEntity::getEmployeeName,
-						Collectors.summingDouble(TimeClockEntity::calcSalary)));
-	System.out.println(map2);
+		Map<String, Double> map = new HashMap<>();
+		for (TimeClockEntity t : timeClocks) {
+			if (!map.containsKey(t.getEmployeeName())) {
+				List<TimeClockEntity> list = new ArrayList<>();
+				list.add(t);
+				map.put(t.getEmployeeName(), t.calcSalary());
+			} else {
+				map.replace(t.getEmployeeName(), map.get(t.getEmployeeName()) + t.calcSalary());
+			}
+		}
+//		Map<String, Double> map = timeClocks.stream().collect(Collectors.groupingBy(TimeClockEntity::getEmployeeName,
+//				Collectors.summingDouble(TimeClockEntity::calcSalary)));
+		System.out.println(map);
 	}
 
 	/**
 	 * 以人名分組
 	 */
 	public static void toMapByName(List<TimeClockEntity> timeClocks) {
-
+		Map<String, TimeClockEntity> map = timeClocks.stream()
+				.collect(Collectors.toMap(TimeClockEntity::getEmployeeName, Function.identity(), (x, y) -> x));
+		System.out.println(map);
 	}
 
 	/**
-	 * 以人名分組
-	 * 並算出每個人合計薪資
-	 * (因為沒有collect成list的動作，因此比上面快)
+	 * 以人名分組 並算出每個人合計薪資 (因為沒有collect成list的動作，因此比上面快)
 	 */
 	public static void toMapByNameWithSumSalary(List<TimeClockEntity> timeClocks) {
-		// TODO
+		Map<String, Double> map = timeClocks.stream().collect(
+				Collectors.toMap(TimeClockEntity::getEmployeeName, TimeClockEntity::calcSalary, (x, y) -> x + y));
+		System.out.println(map);
 	}
 
 	/**
-	 * 根據人名分組，再根據每周分組
-	 * 並算出每人每周的總薪水
+	 * 根據人名分組，再根據每周分組 並算出每人每周的總薪水
 	 */
 	public static void groupingByNameAndWeekWithSumSalary(List<TimeClockEntity> timeClocks) {
-		// TODO
+		Map<String, Map<Integer, Double>> map = timeClocks.stream()
+				.collect(Collectors.groupingBy(TimeClockEntity::getEmployeeName,
+						Collectors.groupingBy(x -> x.getTimeIn().get(WeekFields.ISO.weekOfYear()),
+								Collectors.summingDouble(TimeClockEntity::calcSalary))));
+		System.out.println(map);
 	}
 
 }
--- stream-adv/src/main/java/tw/com/softleader/practice/CountTxtsByYearMonthPracticeApp.java
+++ stream-adv/src/main/java/tw/com/softleader/practice/CountTxtsByYearMonthPracticeApp.java
@@ -7,9 +7,11 @@ import java.nio.file.Paths;
 import java.time.YearMonth;
 import java.time.chrono.MinguoDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.stream.Collectors;
 
 import com.google.common.collect.Lists;
 
@@ -19,22 +21,62 @@ public class CountTxtsByYearMonthPracticeApp {
 	public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyMMdd");
 
 	/**
-	 * 將resource/txts中所有的txt檔的所屬資料夾為期案件編號
-	 * 案件編號以HW開頭者為線上交易進件，TP開頭則代表為台北區的書面掃描進件
+	 * 將resource/txts中所有的txt檔的所屬資料夾為期案件編號 案件編號以HW開頭者為線上交易進件，TP開頭則代表為台北區的書面掃描進件
 	 *
-	 * 案件編號格式說明:
-	 * 線上交易件 HW01061129000001, 其中1061129為進件日期, 格式(民國年)YYYMMDD
-	 * 台北區的書面件 TP060821191749B0, 其中060821為進件日期, 只有民國100年以後的資料, 因此開頭須補1, 格式(民國年)YYYMMDD
+	 * 案件編號格式說明: 線上交易件 HW01061129000001, 其中1061129為進件日期, 格式(民國年)YYYMMDD 台北區的書面件
+	 * TP060821191749B0, 其中060821為進件日期, 只有民國100年以後的資料, 因此開頭須補1, 格式(民國年)YYYMMDD
 	 *
 	 * 請依照月份統計，蒐集每月案件數
+	 * 
 	 * @param args
 	 */
 	public static void main(String[] args) throws Exception {
+		Stream();
+	}
 
+	public static void Stream() throws Exception {
+		final URL txtsUrl = CountTxtsByYearMonthPracticeApp.class.getResource("/txts");
+		final Path txtPath = Paths.get(txtsUrl.toURI());
+		final File[] files = txtPath.toFile().listFiles();
+		final Map<String, List<YearMonth>> result = Arrays.asList(files).stream()
+				.filter(File::isDirectory)
+				.map(x -> x.getName().contains("HW") ? x.getName().substring(3, 10) : 1 + x.getName().substring(2, 8))
+				.map(x -> Parses.tryMinguoDate(x, FORMATTER)).map(YearMonth::from)
+				.collect(Collectors.groupingBy(YearMonth::toString));
+		result.forEach((k, v) -> System.out.println(k + "=" + v.size()));
+//		 Map<YearMonth, Integer> result = new HashMap<>();
+//				Stream<File> tp= Arrays.stream(files).filter(x->x.getName().startsWith("TP"));
+//				Stream<File> hw= Arrays.stream(files).filter(x->x.getName().startsWith("HW"));
+//				tp.filter(File::isDirectory)
+//				.map(x->1+x.getName().substring(2, 8))
+//				.map(x->Parses.tryMinguoDate(x, FORMATTER))
+//				.filter(x->x!=null)
+//				.map(x->YearMonth.from(x))
+//				.forEach(x->{
+//					if(!result.containsKey(x)) {
+//						result.put(x, 1);
+//					}else {
+//						result.replace(x, result.get(x)+1);
+//					}
+//				});
+//				hw.filter(File::isDirectory)
+//				.map(x->x.getName().substring(3, 10))
+//				.map(x->Parses.tryMinguoDate(x, FORMATTER))
+//				.filter(x->x!=null)
+//				.map(x->YearMonth.from(x))
+//				.forEach(x->{
+//					if(!result.containsKey(x)) {
+//						result.put(x, 1);
+//					}else {
+//						result.replace(x, result.get(x)+1);
+//					}
+//				});
+//		result.forEach((k, v) -> System.out.println(k + "=" + v));
 	}
 
 	/**
 	 * 不使用Stream的範例
+	 * 
 	 * @throws Exception
 	 */
 	public static void nonStream() throws Exception {
